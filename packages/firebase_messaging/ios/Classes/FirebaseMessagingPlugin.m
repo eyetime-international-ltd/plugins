fc3ae5fcd3037eedfb0b19d072e6a499a5028a40
Johannes Erlacher <johannes.erlacher@bytepoets.com>
2019-09-03 10:51:49 +0200

    [ETK-460] disable canceling of notifications when app becomes active

--- packages/firebase_messaging/ios/Classes/FirebaseMessagingPlugin.m
+++ packages/firebase_messaging/ios/Classes/FirebaseMessagingPlugin.m
@@ -199,8 +199,9 @@ static FlutterError *getFlutterError(NSError *error) {
   // therefore the next line is setting it to 1 first before clearing it again
   // to remove all
   // notifications.
-  application.applicationIconBadgeNumber = 1;
-  application.applicationIconBadgeNumber = 0;
+  // NOTE: These lines clear all notifications once the app become active
+  // application.applicationIconBadgeNumber = 1;
+  // application.applicationIconBadgeNumber = 0;
 }
 
 - (bool)application:(UIApplication *)application
